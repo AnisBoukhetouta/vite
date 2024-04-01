0543a63779faa03d6b21f807bf0a76949b8eaa3e
TommyHong <ToomyHong1913@outlook.com>
2024-04-01 15:57:28 +0900

    temp

--- src/components/gameView/gameView.tsx
+++ src/components/gameView/gameView.tsx
@@ -4,7 +4,7 @@ import classes from "./gameView.module.css"
 export default function GameView() {
   return (
     <div>
-      <img className={classes.gameImage} src="images/home/10302.jpg" alt="background" />
+      <img className={classes.gameImage} src="/images/home/10302.jpg" alt="background" />
     </div>
   );
 }
